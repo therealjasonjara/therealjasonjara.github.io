3f7ab0331f426bd9488d8ae8c46174fe9cb54b1a
therealjasonjara <therealjasonjara@gmail.com>
2019-03-22 17:25:09 +0800

    Added particles again

--- js/script.js
+++ js/script.js
@@ -1,71 +1,70 @@
-"use strict"
+"use strict";
 
+particlesJS.load("particles-js", "js/particles.json");
 
-// particlesJS.load('particles-js', 'js/particles.json');
-
-function removeLocationHash(){
-  const noHashURL = window.location.href.replace(/#.*$/, '');
-  window.history.replaceState('', document.title, noHashURL) 
+function removeLocationHash() {
+  const noHashURL = window.location.href.replace(/#.*$/, "");
+  window.history.replaceState("", document.title, noHashURL);
 }
-window.addEventListener("popstate", function(event){
+window.addEventListener("popstate", function(event) {
   removeLocationHash();
 });
-window.addEventListener("hashchange", function(event){
+window.addEventListener("hashchange", function(event) {
   event.preventDefault();
   removeLocationHash();
 });
-window.addEventListener("load", function(){
+window.addEventListener("load", function() {
   removeLocationHash();
 });
 
 // Scroll to top button
 function checkScrollPosition() {
-  const button = document.querySelector('.scrollTop')
-  if ((window.pageYOffset) >= 100) { // Check if window is scrolled 100px from top;
-    button.classList.add('scrolled')
+  const button = document.querySelector(".scrollTop");
+  if (window.pageYOffset >= 100) {
+    // Check if window is scrolled 100px from top;
+    button.classList.add("scrolled");
   } else {
-    button.classList.remove('scrolled')
+    button.classList.remove("scrolled");
   }
 }
 
-window.addEventListener('scroll', checkScrollPosition)
+window.addEventListener("scroll", checkScrollPosition);
 
 // Hamburger
-const toggleButton = document.querySelector('.hamburger')
-const buttonIcon = document.querySelector('.hamburger__icon')
-const mobileMenu = document.querySelector('.menu__phone__container')
-const disableScroll = document.getElementById('site-body')
-const toggleAnywhere = document.getElementById('site-content')
+const toggleButton = document.querySelector(".hamburger");
+const buttonIcon = document.querySelector(".hamburger__icon");
+const mobileMenu = document.querySelector(".menu__phone__container");
+const disableScroll = document.getElementById("site-body");
+const toggleAnywhere = document.getElementById("site-content");
 
 let isClicked = false; // Element Boolean
 
 function clickHandler() {
-  if ( !isClicked ) {
+  if (!isClicked) {
     isClicked = true;
-    mobileMenu.classList.add('show')
-    buttonIcon.classList.add('animate')
-    disableScroll.classList.add('menu__open')
-    toggleAnywhere.classList.add('menu__close')
-    
+    mobileMenu.classList.add("show");
+    buttonIcon.classList.add("animate");
+    disableScroll.classList.add("menu__open");
+    toggleAnywhere.classList.add("menu__close");
   } else {
     isClicked = false;
-    mobileMenu.classList.remove('show')
-    buttonIcon.classList.remove('animate')
-    disableScroll.classList.remove('menu__open')
-    toggleAnywhere.classList.remove('menu__close')
+    mobileMenu.classList.remove("show");
+    buttonIcon.classList.remove("animate");
+    disableScroll.classList.remove("menu__open");
+    toggleAnywhere.classList.remove("menu__close");
   }
 }
 
-toggleButton.addEventListener('click', clickHandler)
+toggleButton.addEventListener("click", clickHandler);
 
 // Animate on scroll
-const about = document.getElementsByClassName('about-items');
-const skills = document.getElementsByClassName('skills-items');
-const works = document.getElementsByClassName('works__image');
-const contact = document.getElementsByClassName('contact-us')
+const about = document.getElementsByClassName("about-items");
+const skills = document.getElementsByClassName("skills-items");
+const works = document.getElementsByClassName("works__image");
+const contact = document.getElementsByClassName("contact-us");
 
 // checks if element passed as parameter is in viewport
-const isElementInViewport = (el) => { 
+const isElementInViewport = el => {
   const getBoundValues = el.getBoundingClientRect();
   const windowHeight = window.innerHeight;
   const windowWidth = window.innerWidth;
@@ -74,26 +73,32 @@ const isElementInViewport = (el) => {
     getBoundValues.bottom > 0 &&
     getBoundValues.right > 0 &&
     getBoundValues.left <
-    (windowWidth || document.documentElement.clientWidth) &&
+      (windowWidth || document.documentElement.clientWidth) &&
     getBoundValues.top < (windowHeight || document.documentElement.clientHeight)
   );
-}
+};
 
 window.addEventListener("scroll", () => {
-  for (var item of about) { // for each about-items
-    if (isElementInViewport(item) == true) { // check if item is in viewport
+  for (var item of about) {
+    // for each about-items
+    if (isElementInViewport(item) == true) {
+      // check if item is in viewport
       item.classList.replace("hidden", "scale-up"); // if it is, remove the class that hides it and add in the css animation
     }
   }
 
-  for (var item of skills) { // for each post 
-    if (isElementInViewport(item) == true) { // check if item is in viewport
+  for (var item of skills) {
+    // for each post
+    if (isElementInViewport(item) == true) {
+      // check if item is in viewport
       item.classList.replace("hidden", "scale-up"); // if it is, remove the class that hides it and add in the css animation
     }
   }
 
-  for (var item of works) { // for each post 
-    if (isElementInViewport(item) == true) { // check if item is in viewport
+  for (var item of works) {
+    // for each post
+    if (isElementInViewport(item) == true) {
+      // check if item is in viewport
       item.classList.replace("hidden", "scale-up"); // if it is, remove the class that hides it and add in the css animation
     }
   }
@@ -109,19 +114,18 @@ window.addEventListener("scroll", () => {
   }
 });
 
-
 // Preloader
 
-const preloader = document.querySelector('.preloader__container')
+const preloader = document.querySelector(".preloader__container");
 
 const preloadScroll = () => {
-  window.scrollTo(0, 0)
-}
+  window.scrollTo(0, 0);
+};
 
-window.addEventListener('scroll', preloadScroll)
+window.addEventListener("scroll", preloadScroll);
 
-window.addEventListener('load', function(){
-  preloader.style.opacity = 0
-  preloader.classList.add('hide')
-  window.removeEventListener('scroll', preloadScroll)
-})
+window.addEventListener("load", function() {
+  preloader.style.opacity = 0;
+  preloader.classList.add("hide");
+  window.removeEventListener("scroll", preloadScroll);
+});
